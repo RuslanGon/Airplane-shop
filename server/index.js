42ab38b398b079fa7387a7a81bf4041cfdee9b34
RuslanGon <ruslan441006@gmail.com>
2025-04-25 11:23:10 +0300

    wrer

--- server/index.js
+++ server/index.js
@@ -1,3 +1,23 @@
-import express from 'express'
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import startServer from './db.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const app = express()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use('/static', express.static(path.join(__dirname, '/assets')));
+
+app.get('/', (req, res) => {
+  res.send("hello wordaaa");
+});
+
+startServer(app);
