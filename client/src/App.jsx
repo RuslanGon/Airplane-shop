fb6ec88c3d26601f9f93059190af7c92c9366bf9
RuslanGon <ruslan441006@gmail.com>
2025-04-27 18:13:53 +0300

    wre

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import './App.css'
 
 const App = () => {
   return (
     <div>
       hello word
+      <h1>
+        xcwdcw
+      </h1>
+      <h2>wdcwdc</h2>
     </div>
   )
 }
