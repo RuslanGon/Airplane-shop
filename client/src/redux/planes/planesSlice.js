85813754c6bd649245bb13e69a3cdda7ff1a08a1
RuslanGon <ruslan441006@gmail.com>
2025-04-28 17:04:55 +0300

    wer

--- client/src/redux/planes/planesSlice.js
+++ client/src/redux/planes/planesSlice.js
@@ -1,13 +1,26 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import planesService from '../../store/servicec/planesService.js'
+// import planesService from '../../store/servicec/planesService.js'
+import axios from 'axios';
 
-export const getPlanes = createAsyncThunk('GET_PLANES', async (_, thunkAPI) => {
-try {
-  return planesService.getPlanes()
-} catch (error) {
-  return thunkAPI.rejectWithValue(error.response.data)
-}
-})
+// export const getPlanes = createAsyncThunk("GET_PLANES", async (_, thunkAPI) => {
+//   try {
+//     return planesService.getPlanes();
+//   } catch (error) {
+//     return thunkAPI.rejectWithValue(error.response.data);
+//   }
+// });
+
+export const getPlanes = createAsyncThunk(
+  "GET_PLANES",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('http://localhost:3001/api/planes');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
 
 const initialState = {
   planes: null,
