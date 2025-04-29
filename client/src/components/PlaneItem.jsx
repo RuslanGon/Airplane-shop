3776eea4115de341af57d8c48de5c1e0d90fa1f2
RuslanGon <ruslan441006@gmail.com>
2025-04-29 12:34:00 +0300

    wer

--- client/src/components/PlaneItem.jsx
+++ client/src/components/PlaneItem.jsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import css from './Planes.module.css';
 import { Link } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa';  
 
 const PlaneItem = ({ plane }) => {
   return (
-    <Link to={`/planes/${plane._id}`}  className={css.planeCard}>
-      <img
-        src={plane.planeImage}
-        alt={plane.name}
-        className={css.planeImage}
-      />
-      <h3>{plane.name}</h3>
-      <p>{plane.description}</p>
-      <p>
-        <strong>Цена:</strong> {plane.price} грн.
-      </p>
-      <p>
-        <strong>Вместимость:</strong> {plane.capacity} человек
-      </p>
-    </Link>
+    <div className={css.planeCard}>
+      <Link to={`/planes/${plane._id}`} className={css.link}>
+        <img
+          src={plane.planeImage}
+          alt={plane.name}
+          className={css.planeImage}
+        />
+        <h3>{plane.name}</h3>
+        <p>{plane.description}</p>
+        <p>
+          <strong>Цена:</strong> {plane.price} грн.
+        </p>
+        <p>
+          <strong>Вместимость:</strong> {plane.capacity} человек
+        </p>
+      </Link>
+      <button className={css.btn_delete}>
+        <FaShoppingCart size={20} />
+      </button>
+    </div>
   );
 };
 
